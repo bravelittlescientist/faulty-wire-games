493c3e7e0171f2cad235600d25535388859e9d0c
Eugenia Gabrielova <genia.likes.science@gmail.com>
2013-08-28 05:25:35 -0700

    Adds template and 404 handling

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 import os
 import cherrypy
-from bottle import route, run
+from bottle import error, route, run, template, static_file
 
 @route("/")
 def hello_world():
-    return "Hello World!"
+    return template('faultywire')
+
+@route('/static/<filename:path>')
+def send_static(filename):
+    return static_file(filename, root='static')
+
+@error(404)
+def error404(error):
+    return "404: The droids you are looking for are not here."
 
 run(server="cherrypy", host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
